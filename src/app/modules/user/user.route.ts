7e45e954b35f3574f04bb72abf015bdef9e7dd29
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-31 20:50:48 +0600

    validateRequest middleware created

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import { studentValidation } from '../student/student.validation';
+import validateRequest from '../../middlewares/validateRequest';
+
 
 const router = express.Router();
 
-router.post('/create-student', UserControllers.createStudent);
+
+
+router.post('/create-student',validateRequest(studentValidation.studentValidationSchema), UserControllers.createStudent);
 
 export const UserRoutes = router;
