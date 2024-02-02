42a651fe2a5021b481e69de4f872b6c539935bb8
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-02-03 03:06:28 +0600

    handle mongoose validation error

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -7,6 +7,7 @@ import { ZodError, ZodIssue } from 'zod';
 import { TErrorSource } from '../interface/error';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorHandler:ErrorRequestHandler = (
   err,
@@ -32,6 +33,11 @@ const globalErrorHandler:ErrorRequestHandler = (
     message=simplifiedError?.message;
     errorSources=simplifiedError?.errorSources
     
+  } else if(err?.name === 'ValidationError'){
+    const simplifiedError = handleValidationError(err);
+    statusCode=simplifiedError.statusCode;
+    message=simplifiedError.message;
+    errorSources=simplifiedError.errorSources;
   }
   return res.status(statusCode).json({
     success: false,
