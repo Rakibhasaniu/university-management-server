5a271ab60b1c1afa87cff6a441784047203597e0
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-02-10 17:34:14 +0600

    auth middleware created

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,20 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
+import catchAsync from "../utils/catchAsync";
 
 const validateRequest = (schema: AnyZodObject) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-         try{
-            // console.log('validate schema',req.body)
-            await schema.parseAsync({
-                body:req.body,
-            })
+    return catchAsync( async (req: Request, res: Response, next: NextFunction) => {
+         
             
-             next();
-         } catch(err){
-            // console.log(err);
-          next(err);   
-         }
-    }
+        await schema.parseAsync({
+            body:req.body,
+        })
+        
+         next();
+     
+})
 }
 
 export default validateRequest;
